b970de348dd20e5980e15bc708320402f334c631
Parisa Salmani <55203321+parisasl@users.noreply.github.com>
2022-01-07 17:48:45 +0330

    Add files via upload

---
+++ Logistic Regression.py
@@ -0,0 +1,328 @@
+
+# Environment Setup & Dataset Loading:
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# Preparing & Preprocessing the Data:
+
+# In[4]:
+
+
+def load_dataset():
+    train_dataset = h5py.File('../train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # trainset features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # trainset labels
+
+    test_dataset = h5py.File('../test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # testset features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # testset labels
+
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+
+
+# In[5]:
+
+
+# Loading the data (cat/non-cat)
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+
+
+# In[7]:
+
+
+# Example of a picture
+index = 90
+plt.imshow(train_set_x_orig[index])
+print ("y = " + str(train_set_y[:,index]) + ", it's a '" + classes[np.squeeze(train_set_y[:,index])].decode("utf-8") +  "' picture.")
+
+
+# In[5]:
+
+
+# data shape in test and train sets 
+m_train = train_set_y.shape[1]
+m_test = test_set_y.shape[1]
+num_px = train_set_x_orig.shape[1]
+
+print ("Number of training examples: m_train = " + str(m_train))
+print ("Number of testing examples: m_test = " + str(m_test))
+print ("Height/Width of each image: num_px = " + str(num_px))
+print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+print ("train_set_x shape: " + str(train_set_x_orig.shape))
+print ("train_set_y shape: " + str(train_set_y.shape))
+print ("test_set_x shape: " + str(test_set_x_orig.shape))
+print ("test_set_y shape: " + str(test_set_y.shape))
+
+
+# In[6]:
+
+
+# Reshape the training and test examples
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+
+print ("train_set_x_flatten shape: " + str(train_set_x_flatten.shape))
+print ("train_set_y shape: " + str(train_set_y.shape))
+print ("test_set_x_flatten shape: " + str(test_set_x_flatten.shape))
+print ("test_set_y shape: " + str(test_set_y.shape))
+print ("sanity check after reshaping: " + str(train_set_x_flatten[0:5,0]))
+
+
+# In[7]:
+
+
+#standardize the dataset
+train_set_x = train_set_x_flatten / 255.
+test_set_x = test_set_x_flatten / 255.
+
+
+# ### Building & Training Model:
+
+# In[8]:
+
+
+# sigmoid function
+
+def sigmoid(z):
+    s = 1 / (1 + np.exp(-z))
+    
+    return s
+
+
+# In[9]:
+
+
+print ("sigmoid(0) = " + str(sigmoid(0)))
+print ("sigmoid(9.2) = " + str(sigmoid(9.2)))
+
+
+# In[10]:
+
+
+# initialize_with_zeros
+
+def initialize_with_zeros(dim):
+    
+    w = np.zeros(shape=(dim, 1))
+    b = 0
+    
+    return w, b
+
+
+# In[11]:
+
+
+dim = 2
+w, b = initialize_with_zeros(dim)
+print ("w = " + str(w))
+print ("b = " + str(b))
+
+
+# In[12]:
+
+
+# Implement the cost function and its gradient for forward propagation
+
+def propagate(w, b, X, Y):
+    
+    
+    m = X.shape[1]
+    
+    # FORWARD PROPAGATION (FROM X TO COST)
+    A = sigmoid(np.dot(w.T, X) + b)  # compute activation
+    cost = (- 1 / m) * np.sum(Y * np.log(A) + (1 - Y) * (np.log(1 - A)))  # compute cost
+    
+    
+    # BACKWARD PROPAGATION (TO FIND GRAD)
+    dw = (1 / m) * np.dot(X, (A - Y).T)
+    db = (1 / m) * np.sum(A - Y)
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return grads, cost
+
+
+# In[13]:
+
+
+w, b, X, Y = np.array([[1], [2]]), 2, np.array([[1,2], [3,4]]), np.array([[1, 0]])
+grads, cost = propagate(w, b, X, Y)
+print ("dw = " + str(grads["dw"]))
+print ("db = " + str(grads["db"]))
+print ("cost = " + str(cost))
+
+
+# In[14]:
+
+
+#update the parameters
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+    """
+    This function optimizes w and b by running a gradient descent algorithm
+    """
+    
+    costs = []
+    
+    for i in range(num_iterations):
+        
+        
+        # Cost and gradient calculation
+        grads, cost = propagate(w, b, X, Y)
+        
+        # Retrieve derivatives from grads
+        dw = grads["dw"]
+        db = grads["db"]
+        
+        # update rule
+        w = w - learning_rate * dw  # need to broadcast
+        b = b - learning_rate * db
+        
+        # Record the costs
+        if i % 100 == 0:
+            costs.append(cost)
+        
+        # Print the cost every 100 training examples
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" % (i, cost))
+    
+    params = {"w": w,
+              "b": b}
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return params, grads, costs
+
+
+# In[15]:
+
+
+params, grads, costs = optimize(w, b, X, Y, num_iterations= 100, learning_rate = 0.009, print_cost = False)
+
+print ("w = " + str(params["w"]))
+print ("b = " + str(params["b"]))
+print ("dw = " + str(grads["dw"]))
+print ("db = " + str(grads["db"]))
+
+
+# In[16]:
+
+
+# predict the image is cat or non-cat
+def predict(w, b, X):
+        
+    m = X.shape[1]
+    Y_prediction = np.zeros((1, m))
+    w = w.reshape(X.shape[0], 1)
+    
+    # Compute vector "A" predicting the probabilities of a cat being present in the picture
+    A = sigmoid(np.dot(w.T, X) + b)
+    
+    for i in range(A.shape[1]):
+        # Convert probabilities a[0,i] to actual predictions p[0,i]
+        Y_prediction[0, i] = 1 if A[0, i] > 0.5 else 0
+    
+    return Y_prediction
+
+
+# In[17]:
+
+
+print("predictions = " + str(predict(w, b, X)))
+
+
+# In[18]:
+
+
+def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0.5, print_cost=False):
+    """
+    Builds the logistic regression model by calling the function implemented previously
+    """
+    
+    
+    # initialize parameters with zeros
+    w, b = initialize_with_zeros(X_train.shape[0])
+
+    # Gradient descent
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
+    
+    # Retrieve parameters w and b
+    w = parameters["w"]
+    b = parameters["b"]
+    
+    # Predict test/train set examples
+    Y_prediction_test = predict(w, b, X_test)
+    Y_prediction_train = predict(w, b, X_train)
+
+
+    # Print train/test Errors
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+
+    
+    d = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test, 
+         "Y_prediction_train" : Y_prediction_train, 
+         "w" : w, 
+         "b" : b,
+         "learning_rate" : learning_rate,
+         "num_iterations": num_iterations}
+    
+    return d
+
+
+# ### Evaluating Model:
+
+# In[19]:
+
+
+d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000, learning_rate = 0.005, print_cost = True)
+
+
+# In[20]:
+
+
+# Plot learning curve (with costs)
+costs = np.squeeze(d['costs'])
+plt.plot(costs)
+plt.ylabel('cost')
+plt.xlabel('iterations (per hundreds)')
+plt.title("Learning rate =" + str(d["learning_rate"]))
+plt.show()
+
+
+# In[21]:
+
+
+learning_rates = [0.01, 0.001, 0.0001]
+models = {}
+for i in learning_rates:
+    print ("learning rate is: " + str(i))
+    models[str(i)] = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 1900, learning_rate = i, print_cost = False)
+    print ('\n' + "-------------------------------------------------------" + '\n')
+
+for i in learning_rates:
+    plt.plot(np.squeeze(models[str(i)]["costs"]), label= str(models[str(i)]["learning_rate"]))
+
+plt.ylabel('cost')
+plt.xlabel('iterations')
+
+legend = plt.legend(loc='upper center', shadow=True)
+frame = legend.get_frame()
+frame.set_facecolor('0.90')
+plt.show()
+
